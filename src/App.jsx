e79a8fd5675335b6d75f0a640b0ce60b459b492f
Masonmanshark4 <149852525+Masonmanshark4@users.noreply.github.com>
2024-07-23 14:46:30 -0400

    major face set changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,79 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
-import Footer from './components/Footer';
 import './App.css';
+import selfie from './assets/selfie.jpg';
+import preworkStudyGuide from './assets/prework-study-guide.png';
+import logo from './assets/images/IMG_1175.png';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import AboutMe from './AboutMe';
 
-const App = () => (
-  <Router>
-    <Header />
-    <main>
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </main>
-    <Footer />
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Mason Carr</h1>
+        </header>
+
+        <nav>
+          <ul>
+          <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><Link href="/Resume">Resume</Link></li>
+          </ul>
+        </nav>
+
+        <main>
+          <section id="about">
+            <h2>About Me</h2>
+            <img src={selfie} alt="Self image" />
+            <p>
+              Howdy, I am Mason Carr. First let me say thank you for reading, it'll be hard to convey exactly who I am in a couple of sentences, but I will do my best.
+              I am an aspiring developer, of what I haven't figured it out yet. My initial journey here started with building my first computer back in high school all so I could play games with my friends.
+              To getting into modding games using mod managers, from there I started looking at the code of the mods when things didn't work right to try and fix them on my own.
+              Now playing with the code I understand now, thanks to the Ohio State Coding Bootcamp I have a deeper understanding of HTML, CSS, and JavaScript.
+              After having learned more about web react and development it has given me an outlet to my puzzle-solving fixation, especially problems that get me thinking.
+              The process I use to tackle problems is usually considered simple, look at it from the foundation and check its gears as we work up it.
+              It is thanks to that mindset that allows me to live a simple life, able to discern what is within my control and take every opportunity I can from each moment.
+              Or even enjoy the smallest of things in life to their fullest, from a simple silence to the biggest bash.
+              <strong>Learn more here.</strong>
+            </p>
+          </section>
+
+          <Link to="/portfolio">
+            <section id="portfolio-link">
+              <h2>Portfolio</h2>
+              <div className="portfolio-item">
+                <h3>Prework Study Guide</h3>
+                <p>This was a project everyone in my coding bootcamp, but it was my first from scratch website I collaborated on. 
+                   It is a site meant to display some useful points about the basic languages of coding.</p>
+                <img src={preworkStudyGuide} alt="Project Image" style={{ width: '650px' }} />
+              </div>
+            </section>
+          </Link>
+
+          <section id="contact">
+            <h2>Contact Me</h2>
+            <p>You can reach me at <a href="mailto:masonmanshark80@gmail.com">masonmanshark80@gmail.com</a></p>
+          </section>
+        </main>
+
+        <footer>
+          <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+        </footer>
+
+        <div className="fixed-link">
+          <Link to="/about">
+            <img src={logo} alt="logo" style={{ width: '50px' }} />
+          </Link>
+        </div>
+      </div>
+
+      <Switch>
+        <Route path="/about" component={AboutMe} />
+        <Route path="/portfolio" component={Portfolio} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,51 @@
-// src/pages/Portfolio.jsx
 import React from 'react';
-import Project from '../components/Project';
-import './styles/Portfolio.css';
-import marvel from '../assets/Marvel rocks.gif';
-import weather from '../assets/weather-site.png';
-import quiz from '../assets/quiz-code.png';
-import portfolio from '../assets/Portfolio-pre-launch.png';
+import Project from './Project';
+import marvel from './assets/Marvel Rocks.png';
+import weather from './assets/weather-site.png';
+import quiz from './assets/quiz-code.png';
+import portfolio from './assets/portfolio-pre-launch.png';
 
 const projects = [
-  { title: 'Mother-Music', deployed: 'https://mother-music-b890c71f6170.herokuapp.com/', repo: 'https://github.com/Masonmanshark4/Mother-Music' },
-  { title: 'Before-MySpace', deployed: 'N/A', repo: 'https://github.com/Masonmanshark4/Before-MySpace' },
-  { title: 'A-Marvel-Quiz', image: marvel, deployed: 'https://masonmanshark4.github.io/The-Marvel-Chronicles-Knowledge-Saga/', repo: 'https://github.com/Masonmanshark4/The-Marvel-Chronicles-Knowledge-Saga'},
-  { title: 'Weather-Site', image: weather, deployed: 'https://masonmanshark4.github.io/Weather-Site/', repo: 'https://github.com/Masonmanshark4/Weather-Site'},
-  { title: 'Coding-Quiz', image: quiz, deployed: 'https://masonmanshark4.github.io/Quiz-Code/', repo: 'https://github.com/Masonmanshark4/Quiz-Code'},
-  { title: 'The-Beginning', image: portfolio, deployed: 'https://masonmanshark4.github.io/Portfolio/', repo: 'https://github.com/Masonmanshark4/The-Beginning'}
-  
+  {
+    title: 'Mother-Music',
+    deployed: 'https://mother-music-b890c71f6170.herokuapp.com/',
+    repo: 'https://github.com/Masonmanshark4/Mother-Music',
+    description: 'A music streaming platform to discover and listen to new tracks.'
+  },
+  {
+    title: 'Before-MySpace',
+    deployed: 'N/A',
+    repo: 'https://github.com/Masonmanshark4/Before-MySpace',
+    description: 'A retro social media experience reminiscent of early web days.'
+  },
+  {
+    title: 'A-Marvel-Quiz',
+    image: marvel,
+    deployed: 'https://masonmanshark4.github.io/The-Marvel-Chronicles-Knowledge-Saga/',
+    repo: 'https://github.com/Masonmanshark4/The-Marvel-Chronicles-Knowledge-Saga',
+    description: 'Test your knowledge on Marvel superheroes with this fun quiz.'
+  },
+  {
+    title: 'Weather-Site',
+    image: weather,
+    deployed: 'https://masonmanshark4.github.io/Weather-Site/',
+    repo: 'https://github.com/Masonmanshark4/Weather-Site',
+    description: 'Check the current weather and forecast for any city in the world.'
+  },
+  {
+    title: 'Coding-Quiz',
+    image: quiz,
+    deployed: 'https://masonmanshark4.github.io/Quiz-Code/',
+    repo: 'https://github.com/Masonmanshark4/Quiz-Code',
+    description: 'Challenge yourself with this coding quiz that tests your programming knowledge.'
+  },
+  {
+    title: 'The-Beginning',
+    image: portfolio,
+    deployed: 'https://masonmanshark4.github.io/Portfolio/',
+    repo: 'https://github.com/Masonmanshark4/The-Beginning',
+    description: 'A portfolio showcasing my projects and skills as a developer.'
+  }
 ];
 
 const Portfolio = () => (
@@ -26,6 +57,7 @@ const Portfolio = () => (
         image={project.image}
         deployed={project.deployed}
         repo={project.repo}
+        description={project.description}
       />
     ))}
   </section>
